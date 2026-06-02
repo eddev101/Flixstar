d08406d075f4ca64d1dc528be85b50b0b58ffa60
eddev101 <128457558+eddev101@users.noreply.github.com>
2026-06-02 15:52:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Flixstar',
-        short_name: 'Flixstar',
-        description: 'Movie & TV streaming app',
-        theme_color: '#000000',
-        background_color: '#000000',
-        display: 'standalone',
-        start_url: '/',
-        scope: '/',
-        icons: [
-          {
-            src: '/icon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   base: '/Flixstar/',
   server: {
     host: true,
