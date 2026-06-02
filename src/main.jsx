21f88a875829e13c777db70252ef7c82190279b7
eddev101 <128457558+eddev101@users.noreply.github.com>
2026-06-02 15:55:08 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
 
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
