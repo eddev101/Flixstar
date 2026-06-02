183bfba066b7996b23175098d38fc27bb50724f2
eddev101 <128457558+eddev101@users.noreply.github.com>
2026-06-02 15:43:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/icon.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/icon.png',
             sizes: '512x512',
             type: 'image/png'
           }
